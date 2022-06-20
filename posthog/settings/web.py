hanzoai/insights

commit 0a0512a5ddbe66425e7c0e093153a9fd8af09337

fix: copy console-record.min.js mapping correctly (#10364)

bin/copy-scripts-recorder
@@ -1,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cp node_modules/rrweb/dist/record/rrweb-record.min.js frontend/dist/recorder.js
+cp node_modules/rrweb/dist/record/rrweb-record.min.js.map frontend/dist/recorder.js.map
+sed -i -e s/rrweb-record.min.js.map/recorder.js.map/ frontend/dist/recorder.js
+
+cat node_modules/rrweb/dist/plugins/console-record.min.js >> frontend/dist/recorder.js
+cat node_modules/rrweb/dist/plugins/console-record.min.js.map >> frontend/dist/recorder.js.map
+sed -i -e s/console-record.min.js.map/recorder.js.map/ frontend/dist/recorder.js
+
+sed -i -e 's/\/\/\# sourceMappingURL=recorder/window.rrweb = {record: rrwebRecord}\n\/\/# sourceMappingURL=recorder/g' frontend/dist/recorder.js

commit 0ba35af7a586e1e72a58896544a5cf538f30fb8e

fix: console recording console error (#10375)
* remove dev mode caching that hid error introduced in #10364

* add console-record mapping js file separately

bin/copy-scripts-recorder
@@ -7,7 +7,6 @@ cp node_modules/rrweb/dist/record/rrweb-record.min.js.map frontend/dist/recorder
 sed -i -e s/rrweb-record.min.js.map/recorder.js.map/ frontend/dist/recorder.js
 
 cat node_modules/rrweb/dist/plugins/console-record.min.js >> frontend/dist/recorder.js
-cat node_modules/rrweb/dist/plugins/console-record.min.js.map >> frontend/dist/recorder.js.map
-sed -i -e s/console-record.min.js.map/recorder.js.map/ frontend/dist/recorder.js
+cp node_modules/rrweb/dist/plugins/console-record.min.js.map frontend/dist/console-record.min.js.map
 
 sed -i -e 's/\/\/\# sourceMappingURL=recorder/window.rrweb = {record: rrwebRecord}\n\/\/# sourceMappingURL=recorder/g' frontend/dist/recorder.js

posthog/settings/web.py
@@ -220,7 +220,7 @@ EXCEPTIONS_HOG = {
 
 
 def add_recorder_js_headers(headers, path, url):
-    if url.endswith("/recorder.js"):
+    if url.endswith("/recorder.js") and not DEBUG:
         headers["Cache-Control"] = "max-age=31536000, public"
 
 