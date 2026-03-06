29d45a62ecdae81ba227f342f8b58dd96bf19efd
Zach Kelling <z@zeekay.io>
2026-03-06 12:59:04 -0800

    fix: ship hanzo-insights-node as pre-built JS (no build step needed)

--- common/insights-node/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { PostHog } from 'posthog-node'
-
-/** Hanzo Insights client for Node.js. */
-export class Insights extends PostHog {}
-
-export { PostHog } from 'posthog-node'
-export * from 'posthog-node'
